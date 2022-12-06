5c46656d8109cee6ec4341ae885a7bff9544bcd4
Vicky Seth <38067059+vickyseth9933@users.noreply.github.com>
2022-12-06 09:35:27 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  default = "ami-074dc0a6f6c764218"
+}
+
+variable "instance_type" {
+  default = "t2.nano"
+}
+
+variable "key_name" {
+  default = "prod"
+}
+
+variable "name" {
+  default = "prod-machine"
+}
